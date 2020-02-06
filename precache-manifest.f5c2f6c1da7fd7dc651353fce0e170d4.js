0c1605d739ac9e26f482173276ce1d10be27e7a1
regan <regan@HPLAPTOPINOFFIC>
2020-02-06 02:57:11 -0500

    Updates

---
+++ precache-manifest.f5c2f6c1da7fd7dc651353fce0e170d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00b366125f252d5bae376f4eb313e251",
+    "url": "/reactjs-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "506422762fb8c4dead6c",
+    "url": "/reactjs-monsters-rolodex/static/css/main.d322ba35.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/reactjs-monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactjs-monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "506422762fb8c4dead6c",
+    "url": "/reactjs-monsters-rolodex/static/js/main.b7bc954c.chunk.js"
+  },
+  {
+    "revision": "fac0388cefadb360c49f",
+    "url": "/reactjs-monsters-rolodex/static/js/runtime-main.411a0072.js"
+  }
+]);
